085ddb9a3cbec966a5cc769806e4a8ece226daff
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-26 15:40:14 +0200

    add spring boot app

---
+++ backend/src/test/java/com/mechanix/mechanix/MechanixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechanix.mechanix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
