0b52c3d1b8fc068529990b32691b575e3fbd4708
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-28 18:07:13 +0200

    add logic to discount controller

--- backend/src/main/java/com/mechanix/mechanix/controllers/DiscountController.java
+++ backend/src/main/java/com/mechanix/mechanix/controllers/DiscountController.java
@@ -1,11 +1,56 @@
 package com.mechanix.mechanix.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mechanix.mechanix.exceptions.discount.DiscountNotFoundException;
+import com.mechanix.mechanix.exceptions.product.ProductNotFoundException;
+import com.mechanix.mechanix.models.Discount;
+import com.mechanix.mechanix.models.Product;
+import com.mechanix.mechanix.payloads.requests.models.UpdateDiscount;
+import com.mechanix.mechanix.payloads.requests.models.UpdateProduct;
+import com.mechanix.mechanix.repositories.DiscountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/discount")
 public class DiscountController {
+
+    @Autowired
+    DiscountRepository discountRepository;
+
+    @GetMapping(path="/getDiscounts")
+    @ResponseBody
+    Iterable<Discount> getDiscounts() {
+        return discountRepository.findAll();
+    }
+
+    @PostMapping(path="/addDiscount")
+    String addDiscount (@RequestBody Discount discount) {
+        discountRepository.save(discount);
+        return "Discount added.";
+    }
+
+    @PutMapping("updateDiscount/{id}")
+    String updateDiscount(@RequestBody UpdateDiscount discount,
+                         @PathVariable Long id){
+        return discountRepository.findById(id)
+                .map(discount1 -> {
+                    discount1.setName(discount.getName());
+                    discount1.setDescription(discount.getDescription());
+                    discount1.setDiscount_percent(discount.getDiscount_percent());
+                    discount1.setActive(discount.getActive());
+                    discountRepository.save(discount1);
+                    return "Discount updated.";
+                })
+                .orElseThrow(() -> new DiscountNotFoundException(id));
+    }
+
+    @DeleteMapping("/deleteDiscount/{id}")
+    String deleteDiscount(@PathVariable Long id) {
+        if (!discountRepository.existsById(id)){
+            throw new DiscountNotFoundException(id);
+        }
+        discountRepository.deleteById(id);
+        return "Discount deleted.";
+    }
 }
