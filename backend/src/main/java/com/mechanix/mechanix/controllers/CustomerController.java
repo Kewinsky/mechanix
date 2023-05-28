ca8103c4ea925cd940848a66e2264370d040ee1f
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-28 16:45:32 +0200

    add some initial controllers

---
+++ backend/src/main/java/com/mechanix/mechanix/controllers/CustomerController.java
@@ -0,0 +1,11 @@
+package com.mechanix.mechanix.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerController {
+}
