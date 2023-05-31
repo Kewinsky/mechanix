88409d0b40989be925618d7177e9185850f903cc
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-01 00:22:40 +0200

    add new photos

--- frontend/src/components/Slider/SliderComponent.jsx
+++ frontend/src/components/Slider/SliderComponent.jsx
@@ -1,5 +1,5 @@
 import "./SliderStyles.scss";
-import photo_1 from "../../assets/images/slider_photo_1.png";
+import photo_1 from "../../assets/images/slider_photo_1.jpeg";
 import photo_2 from "../../assets/images/slider_photo_2.jpeg";
 import photo_3 from "../../assets/images/slider_photo_3.jpeg";
 
@@ -7,7 +7,7 @@ const SliderComponent = () => {
   return (
     <div
       id="carouselExampleCaptions"
-      className="carousel carousel-dark slide mb-5"
+      className="carousel carousel-dark slide"
       data-bs-ride="carousel"
     >
       <div className="carousel-indicators">
@@ -35,30 +35,30 @@ const SliderComponent = () => {
       <div className="carousel-inner">
         <div className="carousel-item active">
           <img src={photo_1} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          {/* <div className="carousel-caption d-none d-md-block">
             <h4>Search for Recipes</h4>
             <p className="text-center">
               Nullam efficitur nunc sit risus id ex amet ornare bibendum.
             </p>
-          </div>
+          </div> */}
         </div>
         <div className="carousel-item">
           <img src={photo_2} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          {/* <div className="carousel-caption d-none d-md-block">
             <h4>Filtering & Sorting</h4>
             <p className="text-center">
               Orci varius natoque penatibus et magnis dis parturient montes.
             </p>
-          </div>
+          </div> */}
         </div>
         <div className="carousel-item">
           <img src={photo_3} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          {/* <div className="carousel-caption d-none d-md-block">
             <h4>Choose Recipe and Cook!</h4>
             <p className="text-center">
               Donec ut risus id ex faucibus bibendum a dictum erat condimentum.
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
