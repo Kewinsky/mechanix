8a676e3642f7b8d3e09bb6ad97794b56eb3a2c4a
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-01 11:35:59 +0200

    add login page

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,50 @@
+import "./LoginStyles.scss";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="login-wrapper">
+      <h1 className="login-header">My Account</h1>
+      <form className="login-form">
+        <div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+            className="login-input email"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            required
+            className="login-input"
+          />
+        </div>
+        <div className="login-remember">
+          <input
+            type="checkbox"
+            id="remember"
+            name="remember"
+            className="checkbox"
+          />
+          <label for="remember">Remember Me</label>
+        </div>
+        <div>
+          <button type="submit" className="login-submit">
+            Sign In
+          </button>
+        </div>
+        <div className="login-forgot">
+          <a href="/">Have you forgotten your password?</a>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
