70da2d93fff7742564b0dc22c5a1530410ca72da
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-01 13:42:23 +0200

    add contact us page

--- frontend/src/pages/ContactPage/ContactPage.jsx
+++ frontend/src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,48 @@
+import "./ContactStyles.scss";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="contact-wrapper">
+      <h1 className="contact-header">Contact Us</h1>
+      <p className="contact-paragraph">
+        You can contact us using the form available at the bottom of the page.
+      </p>
+      <form className="contact-form">
+        <div className="contact-form-wrapper">
+          <input
+            type="text"
+            placeholder="First Name"
+            required
+            className="contact-input"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            required
+            className="contact-input"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className="contact-input"
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            required
+            className="contact-input"
+          />
+        </div>
+        <textarea placeholder="Message" required className="contact-input" />
+        <div>
+          <button type="submit" className="contact-submit">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactPage;
