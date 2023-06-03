4b5efff3907ac96ca5014d3e4f8b314d5e068e44
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-04 00:19:24 +0200

    add dashboard tab

--- frontend/src/pages/AccountPage/Dashboard/DashboardTab.jsx
+++ frontend/src/pages/AccountPage/Dashboard/DashboardTab.jsx
@@ -1,5 +1,18 @@
+import "./DashboardStyles.scss";
+
 const DashboardTab = () => {
-  return <div>DashboardTab</div>;
+  return (
+    <div className="dashboard-wrapper">
+      <p className="dashboard-greatings">
+        Hello <span>User</span>,
+      </p>
+      <p className="dashboard-content">
+        From your account dashboard you can view your <span>recent orders</span>
+        , manage your <span>shipping and billing addresses</span>, and edit your
+        password and <span>account details</span>.
+      </p>
+    </div>
+  );
 };
 
 export default DashboardTab;
