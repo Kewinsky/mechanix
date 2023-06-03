4f98725e9926bdfb58fdb2f0e103ca1141900038
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-03 11:38:09 +0200

    add product label

--- frontend/src/components/ProductTile/ProductTileComponent.jsx
+++ frontend/src/components/ProductTile/ProductTileComponent.jsx
@@ -2,8 +2,20 @@ import "./ProductTileStyles.scss";
 import photo from "../../assets/images/default-photo.png";
 import { BsCart, BsHeart, BsEye } from "react-icons/bs";
 const ProductTileComponent = () => {
+  const product = {
+    title: "Title",
+    price: "69.99",
+    discount: "-20%",
+    isSold: false,
+  };
+
   return (
     <div className="product__tile-wrapper">
+      {product.isSold === true ? (
+        <div className="product__tile-label">Sold out</div>
+      ) : product.discount !== null ? (
+        <div className="product__tile-label">{product.discount}</div>
+      ) : null}
       <div className="product__tile-photo">
         <img src={photo} alt="keyboard" />
         <div className="product__tile-overlay">
