158509b8f13b8f5065ef10b94ad9bd8544a9dd61
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-04 22:37:00 +0200

    add orders tab

--- frontend/src/pages/AccountPage/Orders/OrdersTab.jsx
+++ frontend/src/pages/AccountPage/Orders/OrdersTab.jsx
@@ -1,5 +1,57 @@
+import "./OrdersStyles.scss";
+
 const OrdersTab = () => {
-  return <div>OrdersTab</div>;
+  return (
+    <table className="orders-table">
+      <thead className="orders-heading">
+        <tr>
+          <th>ORDER NUMBER</th>
+          <th>DATE</th>
+          <th>STATUS</th>
+          <th>TOTAL</th>
+          <th>ACTIONS</th>
+        </tr>
+      </thead>
+      <tbody className="orders-data">
+        <tr>
+          <td>123456789</td>
+          <td>April 29, 2022</td>
+          <td>Delivered</td>
+          <td>$ 25</td>
+          <td>
+            <button>View Order</button>
+          </td>
+        </tr>
+        <tr>
+          <td>123456789</td>
+          <td>April 29, 2022</td>
+          <td>Delivered</td>
+          <td>$ 25</td>
+          <td>
+            <button>View Order</button>
+          </td>
+        </tr>
+        <tr>
+          <td>123456789</td>
+          <td>April 29, 2022</td>
+          <td>Delivered</td>
+          <td>$ 25</td>
+          <td>
+            <button>View Order</button>
+          </td>
+        </tr>
+        <tr>
+          <td>123456789</td>
+          <td>April 29, 2022</td>
+          <td>Delivered</td>
+          <td>$ 25</td>
+          <td>
+            <button>View Order</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default OrdersTab;
