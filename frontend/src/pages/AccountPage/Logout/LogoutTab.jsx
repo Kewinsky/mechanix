97db20d793cda92d69d3d5ea3373451fe26c4157
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-05 09:12:19 +0200

    add logout tab

--- frontend/src/pages/AccountPage/Logout/LogoutTab.jsx
+++ frontend/src/pages/AccountPage/Logout/LogoutTab.jsx
@@ -1,5 +1,14 @@
+import "./LogoutStyles.scss";
+import { BsBoxArrowRight } from "react-icons/bs";
+
 const LogoutTab = () => {
-  return <div>LogoutTab</div>;
+  return (
+    <div className="logout-wrapper">
+      <button>
+        <BsBoxArrowRight size="20" /> Logout
+      </button>
+    </div>
+  );
 };
 
 export default LogoutTab;
