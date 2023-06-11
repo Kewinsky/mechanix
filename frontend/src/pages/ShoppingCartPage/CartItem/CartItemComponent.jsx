1adf4d7cff3d7be1b0de31caf20db6947e760f27
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-11 23:39:20 +0200

    reuse quantity component

--- frontend/src/pages/ShoppingCartPage/CartItem/CartItemComponent.jsx
+++ frontend/src/pages/ShoppingCartPage/CartItem/CartItemComponent.jsx
@@ -2,47 +2,20 @@ import { useState } from "react";
 import "./CartItemStyles.scss";
 import photo from "../../../assets/images/default-photo.png";
 import { BsFillXCircleFill } from "react-icons/bs";
+import QuantityComponent from "../../../components/Quantity/QuantityComponent";
 
 const CartItem = () => {
   const [quantity, setQuantity] = useState(1);
-
-  const handleAddClick = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const handleSubstractClick = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
 
   return (
     <div className="item-wrapper">
       <img src={photo} alt="keyboard" />
       <div className="title">
-        <p>Title</p>
-        <p className="price">$ 28,00</p>
-        <div className="quantity-component mobile">
-          <button
-            className="quantity-button left"
-            onClick={handleSubstractClick}
-          >
-            -
-          </button>
-          <p>{quantity}</p>
-          <button className="quantity-button right" onClick={handleAddClick}>
-            +
-          </button>
+        <div>
+          <p>Title</p>
+          <p className="price">$ 28,00</p>
         </div>
-      </div>
-      <div className="quantity-component pc">
-        <button className="quantity-button left" onClick={handleSubstractClick}>
-          -
-        </button>
-        <p>{quantity}</p>
-        <button className="quantity-button right" onClick={handleAddClick}>
-          +
-        </button>
+        <QuantityComponent quantity={quantity} setQuantity={setQuantity} />
       </div>
       <button className="remove-button">
         <BsFillXCircleFill size="20" />
