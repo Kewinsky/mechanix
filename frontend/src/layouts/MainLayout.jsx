c00b86636052753d8619a0f6026ec3dfbeabec5e
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-12 13:45:06 +0200

    add sidebar for cart preview

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NavbarComponent from "../components/Navbar/NavbarComponent";
 import FooterComponent from "../components/Footer/FooterComponent";
 import "./MainLayout.scss";
+import PreviewCartComponent from "../components/Sidebar/SidebarComponent";
 
 const MainLayout = ({ children, pageTitle }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
 
   return (
     <>
-      <NavbarComponent />
+      <NavbarComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+      <PreviewCartComponent isOpen={isOpen} setIsOpen={setIsOpen} />
       <main className="main__content">{children}</main>
       <FooterComponent />
     </>
