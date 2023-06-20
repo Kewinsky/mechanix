0b0f36b21363dbee7e7a8b5ad69e118e9071a9fe
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-20 11:30:26 +0200

    add cart repo

---
+++ backend/src/main/java/com/mechanix/mechanix/repositories/CartRepository.java
@@ -0,0 +1,13 @@
+package com.mechanix.mechanix.repositories;
+
+import com.mechanix.mechanix.models.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+
+}
